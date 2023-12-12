3f0cd05648c88a712846b447e41913c70a805cc4
Raymundo Daniel Ramírez Ramos <ray.ramirez@unikino.edu.mx>
2023-12-11 20:43:39 -0700

    Initial commit

---
+++ app/src/main/java/com/example/alertdialogs/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.alertdialogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mShowDialogBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mShowDialogBtn = findViewById(R.id.show_dialog_btn);
+
+        mShowDialogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialAlertDialogBuilder builder = new
+                        MaterialAlertDialogBuilder(MainActivity.this);
+                builder.setTitle("Alert Dialog");
+                builder.setMessage("This is a simple Alert Dialog");
+                builder.show();
+            }
+        });
+    }
+}
